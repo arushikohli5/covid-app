f5c56ad76ece479acd5db4010d2e3f95610593ab
Arushi Kohli <arushi1467.cse18@chitkara.edu.in>
2021-06-24 13:14:12 +0530

    .

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,19 @@
+const URL="https://covid19.mathdro.id/api";
+
 let app=angular.module("MyApp",[]);
 
-app.controller('MyCtrl',($scope)=>{
+app.controller('MyCtrl',($scope, $http)=>{
     //contoller
     $scope.title="Stay home Stay Safe";
+    console.log('App loaded');
+
+    //calling api
+    $http.get(URL).then(
+        (response)=>{
+            console.log(response.data);
+            $scope.all_data=response.data;
+    },
+    (error)=>{
+        console.log(error);
+    });
 });
